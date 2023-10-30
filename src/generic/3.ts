938e51dd8ab27ea0eaaee9cebbca853999d6046d
dolphin <fun53@ukr.net>
2023-10-30 02:31:38 +0200

    generic

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,10 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+// function merge<T extends object>(objA: T, objB: T) {
+//  чому не можна використати один женерик для обох параметрів?
+
+  function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
--- src/generic/7.ts
+++ src/generic/7.ts
@@ -10,10 +10,18 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+// const RoleDescription = {
+//   admin: 'Admin User',
+//   editor: 'Editor User',
+//   guest: 'Guest User',
+// };
+
+type RoleDescription = Record<UserRole, string>;
+
+const roleDescription: RoleDescription = {
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User',
 };
 
 export {};
